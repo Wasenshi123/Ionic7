a9f45d0ae492051e13857bc1b4cdc0bca22eb473
Wasenshi <recka123@gmail.com>
2023-05-17 16:26:24 +0700

    demonstrate bug when list is wrapped inside ngForm

--- Ionic7Test/src/app/home/home.page.ts
+++ Ionic7Test/src/app/home/home.page.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { ModalService } from '../modal.service';
-import { ModalContentComponent } from '../modal-content/modal-content.component';
+import { Data, ModalContentComponent } from '../modal-content/modal-content.component';
 import { ModalContentModule } from '../modal-content/modal-content.module';
 import { ModalBaseModule } from '../modal-base/modal-base.module';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { DataModel } from '../data-model';
 
 @Component({
   selector: 'app-home',
@@ -20,10 +19,11 @@ export class HomePage {
   constructor(private modalService: ModalService) {}
 
   async open() {
-    const data = Array(4).fill(null).map((v, i) => ( { title: 'title' + i, name: 'name' + i  } as DataModel ));
-
-    const modal = await this.modalService.openModal(ModalContentComponent, {
-      mylist: data
+    const mylist = Array(10).fill(null).map((n, i) => {
+      return { id: i, value: i, displayName: 'Test' + i } as Data;
     });
+
+    const data = { optionList: mylist };
+    const modal = await this.modalService.openModal(ModalContentComponent, { mydata: data});
   }
 }
--- Ionic7Test/src/app/modal-content/modal-content.component.ts
+++ Ionic7Test/src/app/modal-content/modal-content.component.ts
@@ -10,13 +10,78 @@ import { DataModel } from '../data-model';
 export class ModalContentComponent implements OnInit {
   
   @Input()
-  mylist: DataModel[] = [];
+  mydata!: { optionList: Data[]; };
 
-  selects: boolean[] = [];
+  isValueType = true;
+  isText = false;
+
+  yesLabel: string = '';
+  noLabel: string = '';
+  textLabel: string = '';
+  numberLabel: string = '';
 
   constructor(private modal: ModalController) {}
 
   ngOnInit() {
-    console.log(this.mylist);
+    
   }
+
+  initReadAssessment() {
+    const yesIndex = this.mydata.optionList.findIndex(x => x.name === "yesLabel");
+    if (yesIndex > -1) {
+      this.yesLabel = this.mydata.optionList[yesIndex].displayName;
+      this.mydata.optionList.splice(yesIndex, 1);
+    }
+    const noIndex = this.mydata.optionList.findIndex(x => x.name === "noLabel");
+    if (noIndex > -1) {
+      this.noLabel = this.mydata.optionList[noIndex].displayName;
+      this.mydata.optionList.splice(noIndex, 1);
+    }
+
+    const textIndex = this.mydata.optionList.findIndex(x => x.name == "textLabel");
+    if (textIndex > -1) {
+      this.textLabel = this.mydata.optionList[textIndex].displayName;
+      this.mydata.optionList.splice(textIndex, 1);
+    }
+
+    const numberIndex = this.mydata.optionList.findIndex(x => x.name == "numberLabel");
+    if (numberIndex > -1) {
+      this.numberLabel = this.mydata.optionList[numberIndex].displayName;
+      this.mydata.optionList.splice(numberIndex, 1);
+    }
+  }
+
+
+  trackOption(index: number, item: Data) {
+    return item.id ?? (item as any).tmpId;
+  }
+
+  addNew() {
+    const newId = this.mydata.optionList.length + 1;
+    this.mydata.optionList.push({ id: newId, displayName: 'New'+newId, value: newId } as Data);
+  }
+
+  swap(first: Data, second: Data) {
+    if (!second || !first) {
+      return;
+    }
+    console.log(second);
+    console.log(first);
+
+    const list = this.mydata.optionList;
+    const firstIndex = list.findIndex(x => x.id === first.id);
+    const secondIndex = list.findIndex(x => x.id === second.id);
+    [list[firstIndex], list[secondIndex]] = [list[secondIndex], list[firstIndex]];
+  }
+
+}
+
+export interface Data {
+  id: number;
+  name: string;
+  displayName: string;
+  value?: number;
+  textValue?: string;
+
+  order: number;
 }
